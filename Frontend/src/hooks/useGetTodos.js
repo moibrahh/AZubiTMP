23d13188e9e63231853f56a65416a26dc77bd238
moibrahh <ibrahimmohammed.im900@gmail.com>
2025-06-12 20:29:14 +0000

    todo final

---
+++ Frontend/src/hooks/useGetTodos.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { CustomErrorAlert } from "../utils/general.js";
+
+const useGetTodos = (setTodos, setNumOfPages, setPage) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchTodos = async (page, limit) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        // `https://fullstack-todolist-upnv.onrender.com/todos?page=${page}&limit=${limit}`
+        `http://localhost:3000/api/gettodos?page=${page}&limit=${limit}`
+
+
+      );
+      const data = await response.json();
+      // setTodos(data.todos); fixed name
+      setTodos(data.todoList);
+      setNumOfPages(data.numOfPages);
+      if (page > data.numOfPages) setPage(data.numOfPages);
+    } catch (error) {
+      CustomErrorAlert(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { fetchTodos, isFetchingTodos: isLoading };
+};
+
+export default useGetTodos;
